0b74fcde14119e3593aea4a5b5c998db8286277f
jbphet <john.blanco@colorado.edu>
2014-09-17 16:50:31 -0600

    Consolidated the code that represents the shape placement board, the area and perimeter indicator, the bucket, the eraser button, and the creator nodes into one composite node.

--- js/explore/view/AreaBuilderExplorationView.js
+++ js/explore/view/AreaBuilderExplorationView.js
@@ -10,44 +10,25 @@ define( function( require ) {
 
   // modules
   var ABSwitch = require( 'SUN/ABSwitch' );
-  var AreaAndPerimeterDisplay = require( 'AREA_BUILDER/explore/view/AreaAndPerimeterDisplay' );
   var AreaBuilderSharedConstants = require( 'AREA_BUILDER/common/AreaBuilderSharedConstants' );
   var Bounds2 = require( 'DOT/Bounds2' );
-  var BucketFront = require( 'SCENERY_PHET/bucket/BucketFront' );
-  var BucketHole = require( 'SCENERY_PHET/bucket/BucketHole' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Checkbox = require( 'SUN/Checkbox' );
-  var Color = require( 'SCENERY/util/Color' );
   var Dimension2 = require( 'DOT/Dimension2' );
   var DimensionsIcon = require( 'AREA_BUILDER/common/view/DimensionsIcon' );
-  var EraserButton = require( 'AREA_BUILDER/common/view/EraserButton' );
   var ExploreIcon = require( 'AREA_BUILDER/explore/view/ExploreIcon' );
+  var ExploreNode = require( 'AREA_BUILDER/explore/view/ExploreNode' );
   var Grid = require( 'AREA_BUILDER/common/view/Grid' );
   var HBox = require( 'SCENERY/nodes/HBox' );
-  var ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
-  var Node = require( 'SCENERY/nodes/Node' );
   var Panel = require( 'SUN/Panel' );
   var ResetAllButton = require( 'SCENERY_PHET/ResetAllButton' );
   var ScreenView = require( 'JOIST/ScreenView' );
-  var Shape = require( 'KITE/Shape' );
-  var ShapeCreatorNode = require( 'AREA_BUILDER/common/view/ShapeCreatorNode' );
-  var ShapePlacementBoardNode = require( 'AREA_BUILDER/common/view/ShapePlacementBoardNode' );
   var VBox = require( 'SCENERY/nodes/VBox' );
   var Vector2 = require( 'DOT/Vector2' );
-  var ShapeView = require( 'AREA_BUILDER/common/view/ShapeView' );
 
   // constants
   var CONTROL_INSET = 15;
   var SPACE_AROUND_SHAPE_PLACEMENT_BOARD = 15;
-  var UNIT_SQUARE_LENGTH = AreaBuilderSharedConstants.UNIT_SQUARE_LENGTH;
-  var SHAPE_CREATOR_OFFSET_POSITIONS = [
-    // Offsets used for initial position of shape, relative to bucket hole center.  Empirically determined.
-    new Vector2( -20 - UNIT_SQUARE_LENGTH / 2, 0 - UNIT_SQUARE_LENGTH / 2 ),
-    new Vector2( -10 - UNIT_SQUARE_LENGTH / 2, -2 - UNIT_SQUARE_LENGTH / 2 ),
-    new Vector2( 9 - UNIT_SQUARE_LENGTH / 2, 1 - UNIT_SQUARE_LENGTH / 2 ),
-    new Vector2( 18 - UNIT_SQUARE_LENGTH / 2, 3 - UNIT_SQUARE_LENGTH / 2 ),
-    new Vector2( 3 - UNIT_SQUARE_LENGTH / 2, 5 - UNIT_SQUARE_LENGTH / 2 )
-  ];
 
   /**
    * @param {AreaBuilderExplorationModel} model
@@ -57,140 +38,23 @@ define( function( require ) {
 
     ScreenView.call( this, { renderer: 'svg', layoutBounds: AreaBuilderSharedConstants.LAYOUT_BOUNDS } );
 
-    // TODO: There is a lot of opportunity for consolidation below.
-
-    // Create the node hierarchy for the single placement board view
-    var singleBoardRoot = new Node();
-    this.addChild( singleBoardRoot );
-    var singleBoardBackLayer = new Node();
-    singleBoardRoot.addChild( singleBoardBackLayer );
-    var singleBoardMovableShapesLayer = new Node( { layerSplit: true } ); // Force the moving shape into a separate layer for performance reasons.
-    singleBoardRoot.addChild( singleBoardMovableShapesLayer );
-    var singleBoardBucketFrontLayer = new Node();
-    singleBoardRoot.addChild( singleBoardBucketFrontLayer );
-    var singleBoardControlsLayer = new Node();
-    singleBoardRoot.addChild( singleBoardControlsLayer );
-
-    // Create the node hierarchy for the dual placement board view
-    var dualBoardRoot = new Node();
-    this.addChild( dualBoardRoot );
-    var dualBoardBackLayer = new Node();
-    dualBoardRoot.addChild( dualBoardBackLayer );
-    var dualBoardMovableShapesLayer = new Node( { layerSplit: true } ); // Force the moving shape into a separate layer for performance reasons.
-    dualBoardRoot.addChild( dualBoardMovableShapesLayer );
-    var dualBoardBucketFrontLayer = new Node();
-    dualBoardRoot.addChild( dualBoardBucketFrontLayer );
-    var dualBoardControlsLayer = new Node();
-    dualBoardRoot.addChild( dualBoardControlsLayer );
-
-    // Add the shape placement boards
-    var leftBoardNode = new ShapePlacementBoardNode( model.leftShapePlacementBoard );
-    dualBoardBackLayer.addChild( leftBoardNode );
-    var rightBoardNode = new ShapePlacementBoardNode( model.rightShapePlacementBoard );
-    dualBoardBackLayer.addChild( rightBoardNode );
-    var centerBoardNode = new ShapePlacementBoardNode( model.centerShapePlacementBoard );
-    window.centerBoardNode = centerBoardNode;
-    singleBoardBackLayer.addChild( centerBoardNode );
-
-    // Add the area and perimeter displays
-    var leftAreaAndPerimeterDisplay = new AreaAndPerimeterDisplay( model.leftShapePlacementBoard.areaProperty,
-      AreaBuilderSharedConstants.GREENISH_COLOR, model.leftShapePlacementBoard.perimeterProperty,
-      Color.toColor( AreaBuilderSharedConstants.GREENISH_COLOR ).colorUtilsDarker( AreaBuilderSharedConstants.PERIMETER_DARKEN_FACTOR ),
-      { centerX: leftBoardNode.centerX, bottom: leftBoardNode.top - SPACE_AROUND_SHAPE_PLACEMENT_BOARD } );
-    dualBoardBackLayer.addChild( leftAreaAndPerimeterDisplay );
-    var rightAreaAndPerimeterDisplay = new AreaAndPerimeterDisplay( model.rightShapePlacementBoard.areaProperty,
-      AreaBuilderSharedConstants.PURPLISH_COLOR, model.rightShapePlacementBoard.perimeterProperty,
-      Color.toColor( AreaBuilderSharedConstants.PURPLISH_COLOR ).colorUtilsDarker( AreaBuilderSharedConstants.PERIMETER_DARKEN_FACTOR ),
-      { centerX: rightBoardNode.centerX, bottom: rightBoardNode.top - SPACE_AROUND_SHAPE_PLACEMENT_BOARD } );
-    dualBoardBackLayer.addChild( rightAreaAndPerimeterDisplay );
-    var centerAreaAndPerimeterDisplay = new AreaAndPerimeterDisplay( model.centerShapePlacementBoard.areaProperty,
-      AreaBuilderSharedConstants.ORANGISH_COLOR, model.centerShapePlacementBoard.perimeterProperty,
-      Color.toColor( AreaBuilderSharedConstants.ORANGISH_COLOR ).colorUtilsDarker( AreaBuilderSharedConstants.PERIMETER_DARKEN_FACTOR ),
-      { centerX: centerBoardNode.centerX, bottom: centerBoardNode.top - SPACE_AROUND_SHAPE_PLACEMENT_BOARD } );
-    singleBoardBackLayer.addChild( centerAreaAndPerimeterDisplay );
-
-    // Add the bucket views
-    var identityTransform = ModelViewTransform2.createIdentity();
-    var leftBucketFront = new BucketFront( model.leftBucket, identityTransform );
-    dualBoardBucketFrontLayer.addChild( leftBucketFront );
-    var leftBucketHole = new BucketHole( model.leftBucket, identityTransform );
-    dualBoardBackLayer.addChild( leftBucketHole );
-    var rightBucketFront = new BucketFront( model.rightBucket, identityTransform );
-    dualBoardBucketFrontLayer.addChild( rightBucketFront );
-    var rightBucketHole = new BucketHole( model.rightBucket, identityTransform );
-    dualBoardBackLayer.addChild( rightBucketHole );
-    var centerBucketFront = new BucketFront( model.centerBucket, identityTransform );
-    singleBoardBucketFrontLayer.addChild( centerBucketFront );
-    var centerBucketHole = new BucketHole( model.centerBucket, identityTransform );
-    singleBoardBackLayer.addChild( centerBucketHole );
-
-    // Add the creator nodes.  These must be added after the bucket holes for proper layering.
-    var rectangleShape = new Shape.rect( 0, 0, UNIT_SQUARE_LENGTH, UNIT_SQUARE_LENGTH );
-    SHAPE_CREATOR_OFFSET_POSITIONS.forEach( function( offset ) {
-      singleBoardBackLayer.addChild( new ShapeCreatorNode( rectangleShape, AreaBuilderSharedConstants.ORANGISH_COLOR,
-        model.addUserCreatedMovableShape.bind( model ), {
-          left: centerBucketHole.centerX + offset.x,
-          top: centerBucketHole.centerY + offset.y
-        } ) );
-      dualBoardBackLayer.addChild( new ShapeCreatorNode( rectangleShape, AreaBuilderSharedConstants.GREENISH_COLOR,
-        model.addUserCreatedMovableShape.bind( model ), {
-          left: leftBucketHole.centerX + offset.x,
-          top: leftBucketHole.centerY + offset.y
-        } ) );
-      dualBoardBackLayer.addChild( new ShapeCreatorNode( rectangleShape, AreaBuilderSharedConstants.PURPLISH_COLOR,
-        model.addUserCreatedMovableShape.bind( model ), {
-          left: rightBucketHole.centerX + offset.x,
-          top: rightBucketHole.centerY + offset.y
-        } ) );
-    } );
-
-    // Add the clear buttons
-    var leftBoardClearButton = new EraserButton( {
-      right: leftBucketFront.right - 3,
-      top: leftBucketFront.bottom + 5,
-      listener: function() { model.leftShapePlacementBoard.releaseAllShapes( true ); }
-    } );
-    dualBoardControlsLayer.addChild( leftBoardClearButton );
-    var rightBoardClearButton = new EraserButton( {
-      right: rightBucketFront.right - 3,
-      top: rightBucketFront.bottom + 5,
-      listener: function() { model.rightShapePlacementBoard.releaseAllShapes( true ); }
-    } );
-    dualBoardControlsLayer.addChild( rightBoardClearButton );
-    var centerBoardClearButton = new EraserButton( {
-      right: centerBucketFront.right - 3,
-      top: centerBucketFront.bottom + 5,
-      listener: function() { model.centerShapePlacementBoard.releaseAllShapes( true ); }
-    } );
-    singleBoardControlsLayer.addChild( centerBoardClearButton );
-
-    // Handle the comings and goings of movable shapes.
-    model.movableShapes.addItemAddedListener( function( addedShape ) {
-      // Create and add the view representation for this shape.
-      var shapeNode = new ShapeView( addedShape );
-      var movableShapesLayer = model.boardDisplayMode === 'single' ? singleBoardMovableShapesLayer : dualBoardMovableShapesLayer;
-      movableShapesLayer.addChild( shapeNode );
-
-      // Move the shape to the front when grabbed by the user.
-      addedShape.userControlledProperty.link( function( userControlled ) {
-        if ( userControlled ) {
-          shapeNode.moveToFront();
-        }
-      } );
-
-      // Add the removal listener for if and when this shape is removed from the model.
-      model.movableShapes.addItemRemovedListener( function removalListener( removedShape ) {
-        if ( removedShape === addedShape ) {
-          movableShapesLayer.removeChild( shapeNode );
-          model.movableShapes.removeItemRemovedListener( removalListener );
-        }
-      } );
-    } );
+    // Create the composite nodes that contain the shape placement board, the readout, the bucket, the shape creator
+    // nodes, and the eraser button.
+    var centerExploreNode = new ExploreNode( model.centerShapePlacementBoard, model.addUserCreatedMovableShape.bind( model ),
+      model.movableShapes, model.centerBucket );
+    this.addChild( centerExploreNode );
+    var leftExploreNode = new ExploreNode( model.leftShapePlacementBoard, model.addUserCreatedMovableShape.bind( model ),
+      model.movableShapes, model.leftBucket );
+    this.addChild( leftExploreNode );
+    var rightExploreNode = new ExploreNode( model.rightShapePlacementBoard, model.addUserCreatedMovableShape.bind( model ),
+      model.movableShapes, model.rightBucket );
+    this.addChild( rightExploreNode );
 
     // Control which board(s), bucket(s), and shapes are visible.
     model.boardDisplayModeProperty.link( function( boardDisplayMode ) {
-      dualBoardRoot.visible = boardDisplayMode === 'dual';
-      singleBoardRoot.visible = boardDisplayMode === 'single';
+      centerExploreNode.visible = boardDisplayMode === 'single';
+      leftExploreNode.visible = boardDisplayMode === 'dual';
+      rightExploreNode.visible = boardDisplayMode === 'dual';
     } );
 
     // Create the icons used on the A-B switch
@@ -248,18 +112,19 @@ define( function( require ) {
       bottom: this.layoutBounds.height - CONTROL_INSET,
       lineWidth: 1,
       listener: function() {
-        leftAreaAndPerimeterDisplay.reset();
-        rightAreaAndPerimeterDisplay.reset();
-        centerAreaAndPerimeterDisplay.reset();
+        centerExploreNode.reset();
+        leftExploreNode.reset();
+        rightExploreNode.reset();
         model.reset();
       }
     } ) );
 
-    // Final layout adjustments
-    controlPanel.top = centerBoardNode.bottom + SPACE_AROUND_SHAPE_PLACEMENT_BOARD;
-    controlPanel.left = centerBoardNode.left;
-    switchPanel.top = centerBoardNode.bottom + SPACE_AROUND_SHAPE_PLACEMENT_BOARD;
-    switchPanel.right = centerBoardNode.right;
+    // Perform final layout adjustments
+    var centerBoardBounds = model.centerShapePlacementBoard.bounds;
+    controlPanel.top = centerBoardBounds.maxY + SPACE_AROUND_SHAPE_PLACEMENT_BOARD;
+    controlPanel.left = centerBoardBounds.minX;
+    switchPanel.top = centerBoardBounds.maxY + SPACE_AROUND_SHAPE_PLACEMENT_BOARD;
+    switchPanel.right = centerBoardBounds.maxX;
   }
 
   return inherit( ScreenView, AreaBuilderExplorationView );
---
+++ js/explore/view/ExploreNode.js
@@ -0,0 +1,136 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * A composite node that depicts a shape placement board, a bucket containing shapes to go on the board, an area and
+ * perimeter readout, and an erase button.  These are consolidated together in this node to avoid code duplication.
+ */
+define( function( require ) {
+    'use strict';
+
+    // modules
+    var AreaAndPerimeterDisplay = require( 'AREA_BUILDER/explore/view/AreaAndPerimeterDisplay' );
+    var AreaBuilderSharedConstants = require( 'AREA_BUILDER/common/AreaBuilderSharedConstants' );
+    var BucketFront = require( 'SCENERY_PHET/bucket/BucketFront' );
+    var BucketHole = require( 'SCENERY_PHET/bucket/BucketHole' );
+    var Color = require( 'SCENERY/util/Color' );
+    var EraserButton = require( 'AREA_BUILDER/common/view/EraserButton' );
+    var inherit = require( 'PHET_CORE/inherit' );
+    var ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
+    var Node = require( 'SCENERY/nodes/Node' );
+    var Shape = require( 'KITE/Shape' );
+    var ShapeCreatorNode = require( 'AREA_BUILDER/common/view/ShapeCreatorNode' );
+    var ShapePlacementBoardNode = require( 'AREA_BUILDER/common/view/ShapePlacementBoardNode' );
+    var ShapeView = require( 'AREA_BUILDER/common/view/ShapeView' );
+    var Vector2 = require( 'DOT/Vector2' );
+
+    // constants
+    var SPACE_AROUND_SHAPE_PLACEMENT_BOARD = 15;
+    var IDENTITY_TRANSFORM = ModelViewTransform2.createIdentity();
+    var UNIT_SQUARE_LENGTH = AreaBuilderSharedConstants.UNIT_SQUARE_LENGTH;
+    var UNIT_RECTANGLE_SHAPE = new Shape.rect( 0, 0, UNIT_SQUARE_LENGTH, UNIT_SQUARE_LENGTH );
+    var SHAPE_CREATOR_OFFSET_POSITIONS = [
+      // Offsets used for initial position of shape, relative to bucket hole center.  Empirically determined.
+      new Vector2( -20 - UNIT_SQUARE_LENGTH / 2, 0 - UNIT_SQUARE_LENGTH / 2 ),
+      new Vector2( -10 - UNIT_SQUARE_LENGTH / 2, -2 - UNIT_SQUARE_LENGTH / 2 ),
+      new Vector2( 9 - UNIT_SQUARE_LENGTH / 2, 1 - UNIT_SQUARE_LENGTH / 2 ),
+      new Vector2( 18 - UNIT_SQUARE_LENGTH / 2, 3 - UNIT_SQUARE_LENGTH / 2 ),
+      new Vector2( 3 - UNIT_SQUARE_LENGTH / 2, 5 - UNIT_SQUARE_LENGTH / 2 )
+    ];
+
+    /**
+     * @param {ShapePlacementBoard} shapePlacementBoard
+     * @param {Function} addShapeToModel - Function for adding a newly created shape to the model.
+     * @param {ObservableArray} movableShapeList - The array that tracks the movable shapes.
+     * @param {Bucket} bucket - Model of the bucket that is to be portrayed
+     * @constructor
+     */
+    function ExploreNode( shapePlacementBoard, addShapeToModel, movableShapeList, bucket ) {
+
+      // Verify that the shape placement board is set up to handle a specific color, rather than all colors, since other
+      // code below depends on this.
+      assert && assert( shapePlacementBoard.colorHandled !== '*' );
+      var shapeColor = Color.toColor( shapePlacementBoard.colorHandled );
+
+      Node.call( this );
+
+      // Create the nodes that will be used to layer things visually.
+      var backLayer = new Node();
+      this.addChild( backLayer );
+      var movableShapesLayer = new Node( { layerSplit: true } ); // Force the moving shape into a separate layer for performance reasons.
+      this.addChild( movableShapesLayer );
+      var bucketFrontLayer = new Node();
+      this.addChild( bucketFrontLayer );
+      var singleBoardControlsLayer = new Node();
+      this.addChild( singleBoardControlsLayer );
+
+      // Add the node that represents the shape placement board.  This is positioned based on this model location, and
+      // all other nodes (such as the bucket) are positioned relative to this.
+      var shapePlacementBoardNode = new ShapePlacementBoardNode( shapePlacementBoard );
+      backLayer.addChild( shapePlacementBoardNode );
+
+      // Add the area and perimeter display
+      this.areaAndPerimeterDisplay = new AreaAndPerimeterDisplay(
+        shapePlacementBoard.areaProperty,
+        shapeColor,
+        shapePlacementBoard.perimeterProperty,
+        shapeColor.colorUtilsDarker( AreaBuilderSharedConstants.PERIMETER_DARKEN_FACTOR ),
+        { centerX: shapePlacementBoardNode.centerX, bottom: shapePlacementBoardNode.top - SPACE_AROUND_SHAPE_PLACEMENT_BOARD }
+      );
+      this.addChild( this.areaAndPerimeterDisplay );
+
+      // Add the bucket view elements
+      var bucketFront = new BucketFront( bucket, IDENTITY_TRANSFORM );
+      bucketFrontLayer.addChild( bucketFront );
+      var bucketHole = new BucketHole( bucket, IDENTITY_TRANSFORM );
+      backLayer.addChild( bucketHole );
+
+      // Add the shape creator nodes.  These must be added after the bucket hole for proper layering.
+      SHAPE_CREATOR_OFFSET_POSITIONS.forEach( function( offset ) {
+        backLayer.addChild( new ShapeCreatorNode( UNIT_RECTANGLE_SHAPE, shapeColor,
+          addShapeToModel, {
+            left: bucketHole.centerX + offset.x,
+            top: bucketHole.centerY + offset.y
+          } ) );
+      } );
+
+      // Add the button that allows the board to be cleared of all shapes.
+      this.addChild( new EraserButton( {
+        right: bucketFront.right - 3,
+        top: bucketFront.bottom + 5,
+        listener: function() { shapePlacementBoard.releaseAllShapes( true ); }
+      } ) );
+
+      // Handle the comings and goings of movable shapes.
+      movableShapeList.addItemAddedListener( function( addedShape ) {
+
+        if ( addedShape.color.equals( shapeColor ) ) {
+
+          // Create and add the view representation for this shape.
+          var shapeNode = new ShapeView( addedShape );
+          movableShapesLayer.addChild( shapeNode );
+
+          // Move the shape to the front of this layer when grabbed by the user.
+          addedShape.userControlledProperty.link( function( userControlled ) {
+            if ( userControlled ) {
+              shapeNode.moveToFront();
+            }
+          } );
+
+          // Add the removal listener for if and when this shape is removed from the model.
+          movableShapeList.addItemRemovedListener( function removalListener( removedShape ) {
+            if ( removedShape === addedShape ) {
+              movableShapesLayer.removeChild( shapeNode );
+              movableShapeList.removeItemRemovedListener( removalListener );
+            }
+          } );
+        }
+      } );
+    }
+
+    return inherit( Node, ExploreNode, {
+      reset: function() {
+        this.areaAndPerimeterDisplay.reset();
+      }
+    } );
+  }
+);
