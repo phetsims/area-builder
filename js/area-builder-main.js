811b25080100290abd0a2bd7b1992404eda0b9b1
jbphet <john.blanco@colorado.edu>
2014-10-03 12:59:07 -0600

    Cleaned up a few issues resulting from the merge of the 1.0 branch with this (master) branch.

--- js/area-builder-main.js
+++ js/area-builder-main.js
@@ -21,7 +21,7 @@ define( function( require ) {
     credits: {
       leadDesign: 'Karina K. R. Hensberry',
       softwareDevelopment: 'John Blanco',
-      designTeam: 'Bryce Gruneich, Amanda McGarry, Ariel Paul, Kathy Perkins, Beth Stade',
+      team: 'Bryce Gruneich, Amanda McGarry, Ariel Paul, Kathy Perkins, Beth Stade',
       qualityAssurance: 'Steele Dalton, Oliver Nix, Oliver Orejola, Arnab Purkayastha,\n Amy Rouinfar, Bryan Yoelin'
     }
   };
