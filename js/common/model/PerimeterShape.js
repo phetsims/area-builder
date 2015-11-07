420b1c315338f1fa57a3c2f8dca9c7ef6e3209d5
samreid <reids@colorado.edu>
2015-11-06 21:07:16 -0700

    updated copyright info using grunt update-copyright-dates (2nd pass), see  https://github.com/phetsims/chipper/issues/403

--- js/common/model/PerimeterShape.js
+++ js/common/model/PerimeterShape.js
@@ -1,4 +1,4 @@
-// Copyright 2014, University of Colorado Boulder
+// Copyright 2014-2015, University of Colorado Boulder
 
 /**
  * Model element that describes a shape in terms of 'perimeter points', both exterior and interior.  The interior
--- js/game/view/GameInfoBanner.js
+++ js/game/view/GameInfoBanner.js
@@ -1,4 +1,4 @@
-// Copyright 2014, University of Colorado Boulder
+// Copyright 2014-2015, University of Colorado Boulder
 
 /**
  * Banner that is used to present information to the user as they work through a challenge.
