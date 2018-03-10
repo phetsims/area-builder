ae13eaa19a76ab5d93ce4c067a8c8d65827c5685
Chris Malley <cmalley@pixelzoom.com>
2018-03-10 13:25:05 -0700

    fix formatting of Array.isArray throughout entire code base, https://github.com/phetsims/tasks/issues/919

--- js/common/model/ShapePlacementBoard.js
+++ js/common/model/ShapePlacementBoard.js
@@ -800,7 +800,7 @@ define( function( require ) {
 
     // @private
     perimeterPointsEqual: function( perimeter1, perimeter2 ) {
-      assert && assert( Array.isArray(perimeter1) && Array.isArray(perimeter2), 'Invalid parameters for perimeterPointsEqual' );
+      assert && assert( Array.isArray( perimeter1 ) && Array.isArray( perimeter2 ), 'Invalid parameters for perimeterPointsEqual' );
       if ( perimeter1.length !== perimeter2.length ) {
         return false;
       }
@@ -811,7 +811,7 @@ define( function( require ) {
 
     // @private
     perimeterListsEqual: function( perimeterList1, perimeterList2 ) {
-      assert && assert( Array.isArray(perimeterList1) && Array.isArray(perimeterList2), 'Invalid parameters for perimeterListsEqual' );
+      assert && assert( Array.isArray( perimeterList1 ) && Array.isArray( perimeterList2 ), 'Invalid parameters for perimeterListsEqual' );
       if ( perimeterList1.length !== perimeterList2.length ) {
         return false;
       }
