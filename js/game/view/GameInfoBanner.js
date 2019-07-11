c8f02ea71950b614bc2250d2665c5244be70660c
zepumph <michael.kauzmann@colorado.edu>
2019-07-11 12:54:16 -0800

    update copyright dates, https://github.com/phetsims/tasks/issues/988

--- js/game/view/GameInfoBanner.js
+++ js/game/view/GameInfoBanner.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * Banner that is used to present information to the user as they work through a challenge.
