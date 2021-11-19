caceed060176e21478feecddd1785e2f9a02d306
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:02 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/areaBuilderStrings.ts
@@ -0,0 +1,45 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import areaBuilder from './areaBuilder.js';
+
+type StringsType = {
+  'area-builder': {
+    'title': string
+  },
+  'aSolutionColon': string,
+  'aSolution': string,
+  'explore': string,
+  'game': string,
+  'area': string,
+  'areaQuestion': string,
+  'perimeter': string,
+  'values': string,
+  'level': string,
+  'pattern': {
+    '0challenge': {
+      '1max': string
+    }
+  },
+  'buildIt': string,
+  'findTheArea': string,
+  'yourGoal': string,
+  'youBuilt': string,
+  'youEntered': string,
+  'areaEquals': string,
+  'perimeterEquals': string,
+  'solutionColon': string,
+  'solution': string,
+  'startOver': string,
+  'invalidValue': string
+};
+
+const areaBuilderStrings = getStringModule( 'AREA_BUILDER' ) as StringsType;
+
+areaBuilder.register( 'areaBuilderStrings', areaBuilderStrings );
+
+export default areaBuilderStrings;
