9d97bbc214f4f57971d18a104e79ad5238d2d2b8
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:40 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/areaBuilderStrings.ts
+++ js/areaBuilderStrings.ts
@@ -5,37 +5,60 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import areaBuilder from './areaBuilder.js';
 
 type StringsType = {
   'area-builder': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'aSolutionColon': string;
+  'aSolutionColonProperty': TReadOnlyProperty<string>;
   'aSolution': string;
+  'aSolutionProperty': TReadOnlyProperty<string>;
   'explore': string;
+  'exploreProperty': TReadOnlyProperty<string>;
   'game': string;
+  'gameProperty': TReadOnlyProperty<string>;
   'area': string;
+  'areaProperty': TReadOnlyProperty<string>;
   'areaQuestion': string;
+  'areaQuestionProperty': TReadOnlyProperty<string>;
   'perimeter': string;
+  'perimeterProperty': TReadOnlyProperty<string>;
   'values': string;
+  'valuesProperty': TReadOnlyProperty<string>;
   'level': string;
+  'levelProperty': TReadOnlyProperty<string>;
   'pattern': {
     '0challenge': {
       '1max': string;
+      '1maxProperty': TReadOnlyProperty<string>;
     }
   };
   'buildIt': string;
+  'buildItProperty': TReadOnlyProperty<string>;
   'findTheArea': string;
+  'findTheAreaProperty': TReadOnlyProperty<string>;
   'yourGoal': string;
+  'yourGoalProperty': TReadOnlyProperty<string>;
   'youBuilt': string;
+  'youBuiltProperty': TReadOnlyProperty<string>;
   'youEntered': string;
+  'youEnteredProperty': TReadOnlyProperty<string>;
   'areaEquals': string;
+  'areaEqualsProperty': TReadOnlyProperty<string>;
   'perimeterEquals': string;
+  'perimeterEqualsProperty': TReadOnlyProperty<string>;
   'solutionColon': string;
+  'solutionColonProperty': TReadOnlyProperty<string>;
   'solution': string;
+  'solutionProperty': TReadOnlyProperty<string>;
   'startOver': string;
+  'startOverProperty': TReadOnlyProperty<string>;
   'invalidValue': string;
+  'invalidValueProperty': TReadOnlyProperty<string>;
 };
 
 const areaBuilderStrings = getStringModule( 'AREA_BUILDER' ) as StringsType;
