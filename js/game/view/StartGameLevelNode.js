928ffd6a026b2913eb2f554611b161b830baf4a9
phet-dev <phetadmin@gmail.com>
2023-09-13 03:10:44 -0600

    update copyright dates from daily grunt work

--- js/game/view/StartGameLevelNode.js
+++ js/game/view/StartGameLevelNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * A node that pretty much fills the screen and that allows the user to select the game level that they wish to play.
