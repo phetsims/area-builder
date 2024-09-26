628bb022eb5841df34e1518b8bb5176ec2458634
Sam Reid <reids@colorado.edu>
2024-09-25 19:32:38 -0600

    Upgrade to ESLint 9 and flat config, see https://github.com/phetsims/chipper/issues/1474

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * ESlint configuration for area-builder.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+// TODO: Rename import parent everywhere in these files, see https://github.com/phetsims/chipper/issues/1451
+import parent from '../chipper/eslint/sim.eslint.config.mjs';
+
+export default [
+  ...parent
+];
